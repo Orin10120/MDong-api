216d333ef5f94a0e6fbc1efc36e389ac96c50275
AdrianIchiro <adrianfahren10@gmail.com>
2025-10-09 23:33:44 +0700

    pemindahan file controller

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Lecturer\Api\LecturerController;
-use App\Http\Controllers\Lecturer\Api\TopicController;
-use App\Http\Controllers\Lecturer\Api\PeriodController;
+use App\Http\Controllers\LecturerController;
+use App\Http\Controllers\TopicController;
+use App\Http\Controllers\PeriodController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
