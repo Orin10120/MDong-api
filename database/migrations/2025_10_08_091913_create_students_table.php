fa2dd8917ced46ca0d50a1cdd0e7a7416f84c346
NoviMariaNatalia <novimaria22@gmail.com>
2025-10-11 15:08:44 +0700

    Update relasi one-to-many students-projects

--- database/migrations/2025_10_08_091913_create_students_table.php
+++ database/migrations/2025_10_08_091913_create_students_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('students', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->integer('student_number')->unique();
+            $table->integer('student_number');
             $table->string('username');
             $table->string('email')->unique();
             $table->string('password');
